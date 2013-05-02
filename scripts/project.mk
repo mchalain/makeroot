352c3b243ed4c52abef4d5a235ad2d11ed34419c
mchalain <marc.chalain@gmail.com>
2013-05-02 09:53:07 +0200

    [BUGFIX]: create .<project>.prj file when the project's build is done

--- scripts/project.mk
+++ scripts/project.mk
@@ -8,31 +8,31 @@ CFLAGS=--sysroot=$(objtree)
 LDFLAGS=--sysroot=$(objtree) -Wl,-rpath=$(objtree)/lib
 export PKG_CONFIG_LIBDIR PKG_CONFIG_PATH PKG_CONFIG_SYSROOT_DIR CFLAGS LDFLAGS
 
-quiet_cmd_configure-project = CONFIGURE $@
+quiet_cmd_configure-project = CONFIGURE $*
 cmd_configure-project = \
-	$(eval sprj-defconfig = $($(notdir $@)-defconfig)) \
-	$(eval sprj-config = $($(notdir $@)-config)) \
-	$(eval sprj-makeflags = $($(notdir $@)-makeflags)) \
+	$(eval sprj-defconfig = $($(notdir $*)-defconfig)) \
+	$(eval sprj-config = $($(notdir $*)-config)) \
+	$(eval sprj-makeflags = $($(notdir $*)-makeflags)) \
 	$(if $(sprj-config), cd $(sprj-src) && $(sprj-config), \
 	$(if $(sprj-defconfig), $(if $(wildcard  $(sprj-src)/.config), ,cp $(sprj-defconfig) $(sprj-src)/.config; $(MAKE) $(sprj-makeflags) -C $(sprj-src) MAKEFLAGS= silentoldconfig)))
 
-quiet_cmd_build-project = BUILD $@
+quiet_cmd_build-project = BUILD $*
 cmd_build-project = \
-	$(eval sprj-build = $($(notdir $@)-build)) \
-	$(eval sprj-makeflags = $($(notdir $@)-makeflags)) \
+	$(eval sprj-build = $($(notdir $*)-build)) \
+	$(eval sprj-makeflags = $($(notdir $*)-makeflags)) \
 	$(if $(sprj-build), $(sprj-build), $(MAKE) $(sprj-makeflags) MAKEFLAGS= -C $(sprj-src) $(target); )
 
-quiet_cmd_install-project = INSTALL $@
+quiet_cmd_install-project = INSTALL $*
 cmd_install-project = \
-	$(eval sprj-install = $($(notdir $@)-install)) \
-	$(eval sprj-makeflags = $($(notdir $@)-makeflags)) \
+	$(eval sprj-install = $($(notdir $*)-install)) \
+	$(eval sprj-makeflags = $($(notdir $*)-makeflags)) \
 	$(if $(sprj-install), $(sprj-install), $(MAKE)  $(sprj-makeflags) MAKEFLAGS= PREFIX=$(objtree) DESTDIR=$(objtree) -C $(sprj-src) install)
 
-$(sort $(subproject-y) $(subproject-m)): 
-	@$(eval sprj-src =  $(addprefix $(src)/,$@$(if $($(notdir $@)-version),-$($(notdir $@)-version)))) \
+$(sort $(subproject-y) $(subproject-m)):  $(obj)/.%.prj: $($(notdir $@)-defconfig)
+	@$(eval sprj-src =  $(addprefix $(src)/,$*$(if $($(notdir $*)-version),-$($(notdir $*)-version)))) \
 	$(call cmd,configure-project)
-	@$(eval sprj-targets = $($(notdir $@)-build-target)) \
-	$(eval sprj-src =  $(addprefix $(src)/,$@$(if $($(notdir $@)-version),-$($(notdir $@)-version)))) \
+	@$(eval sprj-targets = $($(notdir $*)-build-target)) \
+	$(eval sprj-src =  $(addprefix $(src)/,$*$(if $($(notdir $*)-version),-$($(notdir $*)-version)))) \
 	$(if $(sprj-targets), \
 		$(foreach target, $(sprj-targets), $(call cmd,build-project)), \
 		$(call cmd,build-project))
