42fde6b39ecacbbb3845c99291c167377e92bfd5
mchalain <marc.chalain@gmail.com>
2013-05-03 17:31:06 +0200

    [PATCH]: add move command for install

--- scripts/install.mk
+++ scripts/install.mk
@@ -5,6 +5,8 @@ quiet_cmd_mkdir = MKDIR $@
 cmd_mkdir = $(if $(wildcard $(install-target)), ,mkdir -p $(install-target))
 
 cmd_copy = cp -r $(copy) $(install-target)
+cmd_move = mv $(move) $(install-target)
+quiet_cmd_link = LINK $(link)
 cmd_link = ln -s $(link) $(install-target)
 cmd_touch = touch $(install-target)
 
@@ -13,8 +15,8 @@ cmd_chmod = chmod $(chmod) $(install-target)
 
 quiet_cmd_install = INSTALL $@
 cmd_install = $(eval install-target = $(addprefix $(objtree)/,$@)) \
-$(if $(findstring y,$(dir)), $(cmd_mkdir))$(if $(copy), $(cmd_copy), $(if $(link), $(cmd_link), $(if $(findstring y,$(touch)), $(cmd_touch)))) \
-$(if $(chmod), ; $(cmd_chmod))
+$(if $(findstring y,$(dir)), $(cmd_mkdir), $(if $(move), $(cmd_move), $(if $(copy), $(cmd_copy), $(if $(link), $(cmd_link), $(if $(findstring y,$(touch)), $(cmd_touch)))))) \
+$(if $(chmod), ; $(cmd_chmod)$(if $(chown), ; $(cmd_chown)))
 
 $(sort $(install-y)): $(filter-out $(sort $(install-y)), $(sort $(dir $(install-y))))
 	@$(call cmd,install)
