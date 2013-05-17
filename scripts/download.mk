4f7a13afbdb7b49348cfc002f320b3ef47ae10ac
mchalain <marc.chalain@gmail.com>
2013-05-17 11:17:01 +0200

    [PATCH]: <subproject>-version accepts "git" as value and assume the git download

--- scripts/download.mk
+++ scripts/download.mk
@@ -2,25 +2,29 @@ download-copy= | tee $(addprefix $(BUILD_DOWNLOAD_PATH)/, $(notdir $(1)))
 download-wget=wget -O - $(1) $(if $(findstring y,$(BUILD_DOWNLOAD_KEEP_COPY)), $(download-copy))
 download-directory= cat $(addprefix $(BUILD_DOWNLOAD_PATH)/, $(notdir $(1)))
 
-download=$(if $(wildcard $(addprefix $(BUILD_DOWNLOAD_PATH)/, $(notdir $(1)))), $(download-directory), $(download-wget))
-quiet_cmd_download-project = DOWNLOAD $* from $($(notdir $*)-url:"%"=%)
-cmd_download-project = \
+download-short=$(if $(wildcard $(addprefix $(BUILD_DOWNLOAD_PATH)/, $(notdir $(1)))), $(download-directory), $(download-wget))
+quiet_cmd_download-url = DOWNLOAD $* from $($(notdir $*)-url:"%"=%)
+cmd_download-url = \
 	$(eval url = $($(notdir $*)-url:"%"=%)) \
 	$(if $(findstring .gz, $(suffix $(url))), \
-		$(call download, $(url)) | tar -xzf - -C $(src), \
+		$(call download-short, $(url)) | tar -xzf - -C $(src), \
 	$(if $(findstring .bz2, $(suffix $(url))), \
-		$(call download, $(url)) | tar -xjf - -C $(src), \
+		$(call download-short, $(url)) | tar -xjf - -C $(src), \
 	$(if $(findstring .xz, $(suffix $(url))), \
-		$(call download, $(url)) | tar -xJf - -C $(src), \
+		$(call download-short, $(url)) | tar -xJf - -C $(src), \
 	$(if $(findstring :pserver:,$(url)), \
 		cvs -z 9 -d $(url) co $(notdir $*), \
-	$(if $($(notdir $*)-git),$(call cmd_git-project),$(error set $($(notdir $*)-url)))))))
+	$(if $($(notdir $*)-git),$(call cmd_download-git),$(error set $($(notdir $*)-url)))))))
 
-cmd_git-project = \
+quiet_cmd_download-git = DOWNLOAD $* from $($(notdir $*)-git:"%"=%)
+cmd_download-git = \
 	$(eval url = $($(notdir $*)-git:"%"=%)) \
-	$(if $(findstring .git, $(suffix $(url))), git clone $(url) $(src)/$*)
+	git clone $(url) $(src)/$*
 
 $(download-target): $(obj)/.%.dwl:
-	@$(eval sprj-src = $(addprefix $(src)/,$*$(if $($(notdir $*)-version),-$($(notdir $*)-version)))) \
-	$(if $(wildcard  $(sprj-src)), ,$(call cmd,download-project))
+	@$(if $(findstring git,$($(notdir $*)-version)), \
+		$(eval download-cmd=download-git), \
+		$(eval sprj-version=$($(notdir $*)-version)) $(eval download-cmd=download-url)) \
+	$(eval sprj-src = $(addprefix $(src)/,$*$(if $(sprj-version),-$(sprj-version)))) \
+	$(if $(wildcard  $(sprj-src)), ,$(call cmd,$(download-cmd)))
 	@touch $@
--- scripts/project.mk
+++ scripts/project.mk
@@ -20,7 +20,6 @@ cmd_configure-project = \
 	$(eval sprj-defconfig = $($(notdir $*)-defconfig)) \
 	$(eval sprj-mkconfig = $($(notdir $*)-mkconfig)) \
 	$(eval sprj-config = $($(notdir $*)-config)) \
-	$(eval sprj-makeflags = $($(notdir $*)-makeflags)) \
 	$(if $(sprj-config), $(if $(wildcard  $(sprj-src)/$(config_shipped)), ,cd $(sprj-src) && $(sprj-config) ), \
 	$(if $(sprj-mkconfig), $(if $(wildcard  $(sprj-src)/$(config_shipped)), ,$(MAKE) $(sprj-makeflags) CONFIG=$(srctree)/$(CONFIG_FILE) -C $(sprj-src) -f $(srctree)/$(sprj-mkconfig) configure ), \
 	$(if $(sprj-defconfig), $(if $(wildcard  $(sprj-src)/$(config_shipped)), ,cp $(sprj-defconfig) $(sprj-src)/.config; $(MAKE) $(sprj-makeflags) -C $(sprj-src) MAKEFLAGS= silentoldconfig))))
@@ -28,7 +27,6 @@ cmd_configure-project = \
 quiet_cmd_build-project = BUILD $*
 cmd_build-project = \
 	$(eval sprj-build = $($(notdir $*)-build)) \
-	$(eval sprj-makeflags = $($(notdir $*)-makeflags)) \
 	$(if $(sprj-build), $(if $(wildcard  $(sprj-src)/$(build_shipped)), ,cd $(sprj-src) && $(sprj-build) ), \
 	$(if $(sprj-mkbuild), $(if $(wildcard  $(sprj-src)/$(build_shipped)), ,$(MAKE) $(sprj-makeflags) CONFIG=$(srctree)/$(CONFIG_FILE) -C $(sprj-src) -f $(srctree)/$(sprj-mkconfig) build), \
 	$(MAKE) $(sprj-makeflags) MAKEFLAGS= -C $(sprj-src) $(target); ))
@@ -36,17 +34,17 @@ cmd_build-project = \
 quiet_cmd_install-project = INSTALL $*
 cmd_install-project = \
 	$(eval sprj-install = $($(notdir $*)-install)) \
-	$(eval sprj-makeflags = $($(notdir $*)-makeflags)) \
 	$(if $(sprj-install), $(if $(wildcard  $(sprj-src)/$(install_shipped)), ,cd $(sprj-src) && $(sprj-install) ), \
 	$(if $(sprj-mkinstall), $(if $(wildcard  $(sprj-src)/$(install_shipped)), ,$(MAKE) $(sprj-makeflags) CONFIG=$(srctree)/$(CONFIG_FILE) -C $(sprj-src) -f $(srctree)/$(sprj-mkconfig) install), \
 	$(MAKE)  $(sprj-makeflags) MAKEFLAGS= PREFIX=$(objtree) DESTDIR=$(objtree) -C $(sprj-src) install))
 
 $(sort $(subproject-target)):  $(obj)/.%.prj: $($(notdir $@)-defconfig)
-	@$(eval sprj-src =  $(addprefix $(src)/,$*$(if $($(notdir $*)-version),-$($(notdir $*)-version)))) \
-	$(call cmd,configure-project)
-	@$(eval sprj-targets = $($(notdir $*)-build-target)) \
-	$(eval sprj-src =  $(addprefix $(src)/,$*$(if $($(notdir $*)-version),-$($(notdir $*)-version)))) \
-	$(if $(sprj-targets), \
+	$(eval sprj-makeflags = $($(notdir $*)-makeflags))
+	$(if $(findstring git,$($(notdir $*)-version)),,$(eval sprj-version=$($(notdir $*)-version)))
+	$(eval sprj-src =  $(addprefix $(src)/,$*$(if $(sprj-version),-$(sprj-version))))
+	@$(call cmd,configure-project)
+	$(eval sprj-targets = $($(notdir $*)-build-target))
+	@$(if $(sprj-targets), \
 		$(foreach target, $(sprj-targets), $(call cmd,build-project)), \
 		$(call cmd,build-project))
 	@$(call cmd,install-project)
