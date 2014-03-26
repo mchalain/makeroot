e7c79dd690a07d463034b74e4cc9ce74adf59975
mchalain <marc.chalain@gmail.com>
2014-03-26 12:47:51 +0100

    scripts: new target tree architecture
    now the tree is
     out/
     |
     |-- host/
     |
     |-- target/
         |
         |- <target>/
            |
            |--packages/
            |  |-- newlib-2.15...
            |-- sysroot/
            |-- rootfs/
    
    packages: contains the build of each package
    sysroot: contains all headers and libraries files for next builds
    rootfs: contains the target tree result with binaries and initialization file

--- scripts/install.mk
+++ scripts/install.mk
@@ -3,7 +3,7 @@
 
 quiet_cmd_mkdir = MKDIR $@
 cmd_mkdir = $(if $(wildcard $(install-target)), ,mkdir -p $(install-target))
-cmd_mksubdir = cd $(objtree) && $(cmd_mkdir)
+cmd_mksubdir = cd $(install-dest) && $(cmd_mkdir)
 
 cmd_copy = cp -r $(copy) $(install-target)
 cmd_move = mv $(move) $(install-target)
@@ -15,15 +15,29 @@ cmd_generate = mv $(generate) $(install-target)
 
 cmd_chown = chown $(chown) $(install-target)
 cmd_chmod = chmod $(chmod) $(install-target)
+cmd_strip = strip $(install-target)
 
 quiet_cmd_install = INSTALL $@
 cmd_install = $(eval install-target = $@) \
-cd $(objtree) && $(if $(findstring y,$(dir)), $(cmd_mkdir), $(if $(move), $(cmd_move), $(if $(copy), $(cmd_copy), $(if $(link), $(cmd_link), $(if $(findstring y,$(touch)), $(cmd_touch), $(if $(generate), $(cmd_generate))))))) \
-$(if $(chmod), ; $(cmd_chmod)$(if $(chown), ; $(cmd_chown)))
+	cd $(install-dest) && \
+	$(if $(findstring y,$(dir)), $(cmd_mkdir)) \
+	$(if $(move), $(cmd_move)) \
+	$(if $(copy), $(cmd_copy)) \
+	$(if $(link), $(cmd_link)) \
+	$(if $(findstring y,$(touch)), $(cmd_touch)) \
+	$(if $(generate), $(cmd_generate)) \
+	$(if $(findstring y,$(strip)), && $(cmd_strip)) \
+	$(if $(chmod), && $(cmd_chmod)) \
+	$(if $(chown), && $(cmd_chown))
+
+$(rootfs):
+	mkdir -p $@
 
 install-subdirs:=$(sort $(filter-out $(addsuffix /,$(install-y:%/=%)),$(filter-out ./,$(dir $(install-y)))))
-$(install-subdirs):
-	@$(if $(wildcard $(objtree)/$@),,$(eval install-target = $@) $(call cmd,mksubdir))
+$(install-subdirs): $(rootfs)
+	$(eval install-dest = $(rootfs))
+	@$(if $(wildcard $(rootfs)/$@),,$(eval install-target = $@) $(call cmd,mksubdir))
 
 $(sort $(install-y)): $(install-subdirs)
-	@$(if $(wildcard $(objtree)/$@),,$(call cmd,install))
+	$(eval install-dest = $(rootfs))
+	@$(if $(wildcard $(rootfs)/$@),,$(call cmd,install))
